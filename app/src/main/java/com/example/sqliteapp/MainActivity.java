9b877bf0b078e102124ac8ba22072a3ff5254b76
Jonathan Israel Vásquez Vivas <jon2013.p@hotmail.com>
2021-02-01 19:35:36 -0500

    proyecto base e interfaz

---
+++ app/src/main/java/com/example/sqliteapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.sqliteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1, et2, et3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1=(EditText)findViewById(R.id.editText);
+        et2=(EditText)findViewById(R.id.editText2);
+        et3=(EditText)findViewById(R.id.editText3);
+    }
+}
